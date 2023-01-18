cb4afbb66ffffaa569a0e2022bf49a6b8cc827a2
Duzhiev <magomed.duzhiev@gmail.com>
2023-01-18 15:13:11 +0300

    Добавил некоторые функции в Бэк

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,8 @@ const { Router } = require("express");
 const router = Router();
 
 router.use("/", require("./Users.route"));
-router.use("/goods" , require("./Goods.route"));
+router.use("/goods", require("./Goods.route"));
 router.use("/", require("./Category.route"));
+router.use(require("./Coach.route"));
 
 module.exports = router;
