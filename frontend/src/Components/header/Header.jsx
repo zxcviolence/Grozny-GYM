37135a0c23495fc979fcf57a317327a1d8a04624
DexterYBN <yakubov959@mail.ru>
2023-01-19 13:31:55 +0300

    окна авторизации

--- frontend/src/Components/header/Header.jsx
+++ frontend/src/Components/header/Header.jsx
@@ -3,15 +3,22 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import { GiHamburgerMenu } from "react-icons/gi";
 import styles from "./Header.module.scss";
 import logo from "../../images/logo.png";
-import SignIn from "../SignIn/SignIn";
-import SignUp from "../SignUp/SignUp";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { VscAccount } from "react-icons/vsc";
+import { BiLogOut } from "react-icons/bi";
 
 const Header = () => {
   const [show, setShow] = useState(false);
-  const [modalShow, setModalShow] = React.useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const token = localStorage.getItem("token");
+  const login = useSelector((state) => state.users.login);
+
+  const clearToken = () => {
+    window.location.reload();
+    localStorage.clear(token);
+  };
 
   return (
     <header>
@@ -42,7 +49,23 @@ const Header = () => {
             Menu
           </Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body className={styles.offcanvas_body}></Offcanvas.Body>
+        <Offcanvas.Body className={styles.offcanvas_body}>
+          <div className={styles.auth}>
+            <Link to="/login" hidden={token}>
+              Sign In
+            </Link>
+            {token && (
+              <div className={styles.username}>
+                <VscAccount className={styles.userIcon} />
+                {login} |
+                <button className={styles.exitButton} onClick={clearToken}>
+                  Log out
+                  <BiLogOut className={styles.logoutIcon} />
+                </button>
+              </div>
+            )}
+          </div>
+        </Offcanvas.Body>
       </Offcanvas>
     </header>
   );
