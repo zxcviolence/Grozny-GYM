56d5bad03bac641ed4ccedee9724da23232357f7
DexterYBN <yakubov959@mail.ru>
2023-01-25 17:35:20 +0300

    доработал форму

--- frontend/src/Components/Coach/Coach.jsx
+++ frontend/src/Components/Coach/Coach.jsx
@@ -3,10 +3,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchCoaches } from "../../features/coachSlice";
 import { postForm } from "../../features/coachformSlice";
 import styles from "./Coach.module.scss";
-import Button from "react-bootstrap/Button";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Modal from "react-bootstrap/Modal";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 const Coach = () => {
@@ -14,112 +13,22 @@ const Coach = () => {
   const coaches = useSelector((state) => state.coach.coaches);
   const error = useSelector((state) => state.coach.error);
   const loading = useSelector((state) => state.coach.loading);
-
   const [name, setName] = useState("");
   const [phone, setphone] = useState("");
   const [weight, setWeight] = useState("");
   const [isSport, setIsSport] = useState("");
-  const [modalShow, setModalShow] = useState(false);
   const [select, setSelect] = useState({
     _id: "63c7f17453f3e15520a46af8",
   });
-
-  const handleSetName = (e) => {
-    setName(e.target.value);
-  };
-  const handleSetPhone = (e) => {
-    setphone(e.target.value);
-  };
-  const handleSetWeight = (e) => {
-    setWeight(e.target.value);
-  };
-  const handleSetSport = (e) => {
-    setIsSport(e.target.value);
-  };
-
-  const handleForm = (e) => {
-    e.preventDefault();
-    dispatch(postForm({ name, phone, weight, isSport }));
-  };
 
   useEffect(() => {
     dispatch(fetchCoaches());
   }, [dispatch]);
 
-  function MyVerticallyCenteredModal(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Typography component="h1" variant="h5">
-            Заполните форму
-          </Typography>
-        </Modal.Header>
-        <Modal.Body>
-          <Box component="form" noValidate onSubmit={handleForm} sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="name"
-              label="Имя"
-              name="name"
-              autoComplete="name"
-              onChange={handleSetName}
-              value={name}
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="tel"
-              label="Телефон"
-              type="tel"
-              id="tel"
-              autoComplete="tel"
-              onChange={handleSetPhone}
-              value={phone}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="weight"
-              label="Вес"
-              type="tel"
-              id="weight"
-              autoComplete="weight"
-              onChange={handleSetWeight}
-              value={weight}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="sport"
-              label="Каким спортом занимались?"
-              type="text"
-              id="sport"
-              autoComplete="sport"
-              onChange={handleSetSport}
-              value={isSport}
-            />
-            <Button
-              type="submit"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Отправить
-            </Button>
-          </Box>
-        </Modal.Body>
-      </Modal>
-    );
-  }
+  const handleForm = (e) => {
+    e.preventDefault();
+    dispatch(postForm({ name, phone, weight, isSport }));
+  };
 
   return (
     <main>
@@ -134,19 +43,73 @@ const Coach = () => {
               />
               <div className={styles.secondDiv}>
                 <div className={styles.name}>{coach.name}</div>
-                <Button
-                  className={styles.btn}
-                  onClick={() => setModalShow(true)}
-                >
-                  Записаться
-                </Button>
-
-                <MyVerticallyCenteredModal
-                  show={modalShow}
-                  onHide={() => setModalShow(false)}
-                />
-
                 <div className={styles.description}>{coach.description}</div>
+              </div>
+              <div className={styles.form}>
+                <Typography component="h1" variant="h5">
+                  Чтобы записаться заполните форму
+                </Typography>
+                <Box
+                  component="form"
+                  noValidate
+                  onSubmit={handleForm}
+                  sx={{ mt: 1 }}
+                >
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Ваше имя"
+                    name="name"
+                    autoComplete="name"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="tel"
+                    label="Контактный телефон"
+                    type="tel"
+                    id="tel"
+                    autoComplete="tel"
+                    onChange={(e) => setphone(e.target.value)}
+                    value={phone}
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="weight"
+                    label="Ваш вес"
+                    type="text"
+                    id="weight"
+                    autoComplete="weight"
+                    onChange={(e) => setWeight(e.target.value)}
+                    value={weight}
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="isSport"
+                    label="Какими видами спорта занимались?"
+                    type="text"
+                    id="isSport"
+                    autoComplete="isSport"
+                    onChange={(e) => setIsSport(e.target.value)}
+                    value={isSport}
+                  />
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Записаться
+                  </Button>
+                </Box>
               </div>
             </div>
           );
