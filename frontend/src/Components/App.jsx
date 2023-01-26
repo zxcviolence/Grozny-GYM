7426d991978e0f5206a87dd222d73d369ebedcba
WetFlamer <xamidchabaev04@gmail.com>
2023-01-26 16:12:59 +0300

    bug fix

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -15,6 +15,7 @@ import FormV from '../Components/Job/FormV'
 import Footer from "./Footer/Footer";
 import Admin from "./AdminPanel/Admin";
 import EditUser from "./AdminPanel/EditUser/EditUser";
+import Job from "./Job/Job";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
         <Route path="/login" element={<SignIn />} />
         <Route path="/registration" element={<SignUp />} />
         <Route path="/coaches" element={<Coach />} />
+        <Route path="/job" element={<Job />} />
         <Route path="/sports_bar" element={<SportsBar />} />
         <Route path="/sports_bar/:id" element={<OneElementBar />} />
         <Route path="/admin/edituser" element={<EditUser />} />
